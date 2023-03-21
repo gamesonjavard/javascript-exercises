0845216bad108c247aaea5a00dbf3eeb92e1617b
jgavard <jamesongavard@live.com>
2023-03-21 09:40:20 -0400

    var bigValue selects larger of parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,8 @@
 const sumAll = function(num1, num2) {
     let error = "ERROR";
     let finalSum = 0;
-    let bigValue = Math.max(num1, num2);
+    let bigValue = Math.max(num1, num2);//chooses larger of 2 parameters
+    let smallValue = Math.min(num1, num2);//chooses smaller of 2 parameters
 }
     
     
